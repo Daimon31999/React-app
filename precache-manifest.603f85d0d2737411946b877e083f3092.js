5e9fa81f0acb3940a77ffedc40316fa06fd1e045
Daimon31999 <dimahinev31999@gmail.com>
2020-06-11 17:34:09 +0300

    Updates

---
+++ precache-manifest.603f85d0d2737411946b877e083f3092.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c5bcf958b8fb7c616f24c02441359f",
+    "url": "/react.js-social-network/index.html"
+  },
+  {
+    "revision": "987555352724d1dbd91c",
+    "url": "/react.js-social-network/static/css/main.3f970772.chunk.css"
+  },
+  {
+    "revision": "fa6065fc940078a3c6a7",
+    "url": "/react.js-social-network/static/js/2.50b90d16.chunk.js"
+  },
+  {
+    "revision": "8cb10be61483d03724e6adf68bf2c7d9",
+    "url": "/react.js-social-network/static/js/2.50b90d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987555352724d1dbd91c",
+    "url": "/react.js-social-network/static/js/main.c488e817.chunk.js"
+  },
+  {
+    "revision": "05f6db58b5ca70e88249",
+    "url": "/react.js-social-network/static/js/runtime-main.5282fc14.js"
+  },
+  {
+    "revision": "be49055801a9d09f688bce8576fda9dc",
+    "url": "/react.js-social-network/static/media/profile_photo.be490558.jpg"
+  },
+  {
+    "revision": "18af6f6f1e8be88aeb4913d2a465bfe6",
+    "url": "/react.js-social-network/static/media/react.18af6f6f.png"
+  }
+]);
